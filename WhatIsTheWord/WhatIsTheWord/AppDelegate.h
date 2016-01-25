f8032f1a5b5929b8fe7fece91f671e0d2f164bf5
Konstantina <missytoy@gmail.com>
2016-01-24 20:46:17 -0800

    initial commit

---
+++ WhatIsTheWord/WhatIsTheWord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhatIsTheWord
+//
+//  Created by Konstantina on 1/24/16.
+//  Copyright © 2016 Konstantina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhatIsTheWord/WhatIsTheWord/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WhatIsTheWord
+//
+//  Created by Konstantina on 1/24/16.
+//  Copyright © 2016 Konstantina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
